50413c5419de12579d2db60d812750f6c6c7335b
像科比样战斗 <1909785965@qq.com>
2020-08-21 11:45:32 -0700

    Xcode use token

--- ExtendsTest/ExtendsTest/AppDelegate.m
+++ ExtendsTest/ExtendsTest/AppDelegate.m
@@ -14,7 +14,7 @@
 
 @implementation AppDelegate
 
-//hello github
+//hello github user token
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     return YES;
